680b29d1a55eec643c203581b44ccfa7739c6a42
BekirCanTurkmen <bekircan-2000@hotmail.com>
2023-12-22 22:00:21 +0300

    update

--- Game.java
+++ Game.java
@@ -2,7 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Game {
     Deck deck = new Deck();
-    Main win = new Main();
+    Card win = new Card();
     private Card []boardplayer = new Card[9];
     private Card[] boardComp =new Card [9];
     private int playersum =0;
@@ -10,9 +10,15 @@ public class Game {
     private int counter =0;
     private int playerboardcounter = 0;
     private int compboardcounter = 0;
-    private int gameFinisher = 0;
+    private int playerhelp = 0;
 
+    public int getPlayerhelp() {
+        return playerhelp;
+    }
 
+    public void setPlayerhelp(int playerhelp) {
+        this.playerhelp = playerhelp;
+    }
 
     Scanner sc = new Scanner(System.in);
     public void Play() {
@@ -68,64 +74,21 @@ public class Game {
                 System.out.print((i+1) + ")"+"X ");
             }
         }
-        CardSelectPlayer();
     }
 
     public void CardSelectPlayer() {
         Scanner sc = new Scanner(System.in);
-        /*System.out.println();
-        System.out.println("Choose a card position to play or enter '0' to take card from the deck");
-        int selected = sc.nextInt();
-        // TRY CATCH YAZ 0 DEN KÜCÜK VEYA 4TEN BUYUKSE veya stringse TEKRAR İSTESİN
-        if(selected == 0) {
-            while (true) {
-                if(deck.getDeck()[counter] != null) {
-                    boardplayer[playerboardcounter] = deck.getDeck()[counter];
-                    deck.getDeck()[counter] = null;
-                    playerboardcounter++;
-                    break;
-                }
-                counter++;
-            }
-        } else if ( selected-1>=0 && selected-1<4 ) {
-            //NULL MU KONTROL ET NULL SE NULL SECMEYENE KADAR TEKRAR İSTE(WHİLE DA KULLANABİLİRSİN)
-
-            if (deck.getDeckplayerSeletcted()[selected - 1] == null ) {
-                System.out.println("your choice is null may u choose again");
-                 }
-
-            else{
-                if (deck.getDeckplayerSeletcted()[selected - 1].getSign().equals("x2")) {
-                    boardplayer[playerboardcounter] = boardplayer[playerboardcounter - 1];
-                    deck.getDeckplayerSeletcted()[selected - 1] = null;
-                    playerboardcounter++;
-                } else if (deck.getDeckplayerSeletcted()[selected - 1].getSign().equals("+/-")) {
-                    if (boardplayer[playerboardcounter - 1].getSign().equals("+")) {
-                        boardplayer[playerboardcounter - 1].setSign("-");
-                    } else if (boardplayer[playerboardcounter - 1].getSign().equals("-")) {
-                        boardplayer[playerboardcounter - 1].setSign("+");
-                    }
-                    deck.getDeckplayerSeletcted()[selected - 1] = null;
-                } else {
-                    boardplayer[playerboardcounter] = deck.getDeckplayerSeletcted()[selected - 1];
-                    deck.getDeckplayerSeletcted()[selected - 1] = null;
-                    playerboardcounter++;
-                }
-
-            } }
-
-         */
         int selected;
 
         do {
             try {
                 System.out.println();
-                System.out.println("Choose a card position to play or enter '0' to take a card from the deck");
+                System.out.println("Choose a card position to play or enter '0' to take a card from the deck or enter 5 to stay");
                 String input = sc.nextLine();
                 selected = Integer.parseInt(input);
 
                 if (selected == 0) {
-                    // 0 seçeneğinde kart al
+                    setPlayerhelp(1);
                     while (true) {
                         if (deck.getDeck()[counter] != null) {
                             boardplayer[playerboardcounter] = deck.getDeck()[counter];
@@ -157,8 +120,82 @@ public class Game {
                             deck.getDeckplayerSeletcted()[selected - 1] = null;
                             playerboardcounter++;
                         }
+                    }setPlayerhelp(5);
+                    if(compsum<16) {
+                        counter =0;
+                        for(int i=0;i<deck.getDeckcompSeletcted().length;i++) {
+                            if(deck.getDeckcompSeletcted()[i] != null) {
+                                if(compsum + deck.getDeckcompSeletcted()[i].getNum() == 20 || compsum + deck.getDeckcompSeletcted()[i].getNum() ==19) {
+                                    boardComp[compboardcounter] = deck.getDeckcompSeletcted()[i];
+                                    compsum += deck.getDeckcompSeletcted()[i].getNum();
+                                    compboardcounter++;
+                                    deck.getDeckcompSeletcted()[i] = null;
+
+                                }
+                            }
+                        }
+                        boolean x = true;
+                        while(x==true) {
+                            if(deck.getDeck()[counter] != null) {
+                                boardComp[compboardcounter] = deck.getDeck()[counter];
+                                compsum += boardComp[compboardcounter].getNum();
+                                deck.getDeck()[counter] = null;
+                                compboardcounter++;
+                                if (compsum>16) {
+                                    x=false;
+                                }
+                            }
+                            counter++;
+                        }
+
                     }
-                } else {
+                } else if(selected == 5){
+                    setPlayerhelp(5);
+                    if(compsum<16) {
+                        counter =0;
+                        for(int i=0;i<deck.getDeckcompSeletcted().length;i++) {
+                            if(deck.getDeckcompSeletcted()[i] != null) {
+                                if(compsum + deck.getDeckcompSeletcted()[i].getNum() == 20 || compsum + deck.getDeckcompSeletcted()[i].getNum() ==19) {
+                                    boardComp[compboardcounter] = deck.getDeckcompSeletcted()[i];
+                                    compsum += deck.getDeckcompSeletcted()[i].getNum();
+                                    compboardcounter++;
+                                    deck.getDeckcompSeletcted()[i] = null;
+
+                                }
+                            }
+                        }
+                        boolean x = true;
+                        while(x==true) {
+                            if(deck.getDeck()[counter] != null) {
+                                boardComp[compboardcounter] = deck.getDeck()[counter];
+                                compsum += boardComp[compboardcounter].getNum();
+                                deck.getDeck()[counter] = null;
+                                compboardcounter++;
+                                if (compsum>16) {
+                                    x=false;
+                                }
+                            }
+                            counter++;
+                        }
+
+                    }
+
+
+                    System.out.println();
+                    System.out.println("***************************");
+                    System.out.println("Computer Board");
+                    System.out.println("---------------------------");
+                    for (int i = 0; i < boardComp.length ; i++) {
+                        if(boardComp[i] != null) {
+                            if (boardComp[i].getColor() == null) {
+                                System.out.print(boardComp[i].getSign()+ " ");
+                            }
+                            System.out.print(boardComp[i].getColor() + boardComp[i].getNum() + boardComp[i].getSign()+" ");
+                        }
+                    }
+                    System.out.println();
+                    System.out.println("*************************");
+                }else {
                     System.out.println("Please enter a valid input (0 to take a card or 1-4 to play a card)");
                     selected = -1; // Geçersiz giriş durumunda döngüyü tekrarlamak için -1 atıyoruz
                 }
@@ -166,7 +203,7 @@ public class Game {
                 System.out.println("Invalid input. Please enter a number.");
                 selected = -1; // Geçersiz giriş durumunda döngüyü tekrarlamak için -1 atıyoruz
             }
-        } while (selected < 0 || selected > 4); // Doğru giriş yapana kadar döngüyü devam ettir
+        } while (selected < 0 || selected > 5); // Doğru giriş yapana kadar döngüyü devam ettir
 
 
         for (int i = 0;i<boardplayer.length;i++) {
@@ -191,71 +228,79 @@ public class Game {
         // KONTROL ET
         int yesno;
         System.out.println();
-        do {
-            try {
-                System.out.println("Do you want to play a card or take a card from the deck?");
-                System.out.println("1) YES 2) NO");
-                String input = sc.nextLine();
-                yesno = Integer.parseInt(input);
+        if(getPlayerhelp() != 5) {
+            do {
+                try {
+                    System.out.println("Do you want to play a card or take a card from the deck?");
+                    System.out.println("1) YES 2) NO");
+                    String input = sc.nextLine();
+                    yesno = Integer.parseInt(input);
 
-                if (yesno == 1) {
-                    CardSelectPlayer();
-                } else if (yesno == 2) {
-                    CardSelectComp();
-                    EndGame();
-                    return;
-                } else {
-                    System.out.println("Please enter a valid option (1 for YES, 2 for NO).");
+                    if (yesno == 1) {
+                        CardSelectPlayer();
+                    } else if (yesno == 2) {
+
+                    } else {
+                        System.out.println("Please enter a valid option (1 for YES, 2 for NO).");
+                        yesno = -1; // Set invalid input to repeat the loop
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Please enter a number (1 for YES, 2 for NO).");
                     yesno = -1; // Set invalid input to repeat the loop
                 }
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please enter a number (1 for YES, 2 for NO).");
-                yesno = -1; // Set invalid input to repeat the loop
-            }
-        } while (yesno < 1 || yesno > 2);
-    }
-        /*System.out.println("Do you want to play card or take a card from the deck");
-        System.out.println("1)YES 2)NO ");
-        int yesno = sc.nextInt();
-        if(yesno == 1) {
-            CardSelectPlayer();
-        } else {
-            CardSelectComp();
-            EndGame();
-            return;
+            } while (yesno < 1 || yesno > 2);
         }
-        CardSelectComp();
     }
 
-         */
+
+
+
 
     public void CardSelectComp() {
         //BILGISAYAR ATMAMASI GEREKEN YERDE KART ATIYO KONTROL ET
-
-        counter =0;
-        for(int i=0;i<deck.getDeckcompSeletcted().length;i++) {
-            if(deck.getDeckcompSeletcted()[i] != null) {
-                if(compsum + deck.getDeckcompSeletcted()[i].getNum() == 20 || compsum + deck.getDeckcompSeletcted()[i].getNum() ==19) {
-                    boardComp[compboardcounter] = deck.getDeckcompSeletcted()[i];
-                    compsum += deck.getDeckcompSeletcted()[i].getNum();
-                    compboardcounter++;
-                    deck.getDeckcompSeletcted()[i] = null;
-                    EndGame();
-                    return;
+        if(compsum<16) {
+            counter =0;
+            for(int i=0;i<deck.getDeckcompSeletcted().length;i++) {
+                if(deck.getDeckcompSeletcted()[i] != null) {
+                    if(compsum + deck.getDeckcompSeletcted()[i].getNum() == 20 || compsum + deck.getDeckcompSeletcted()[i].getNum() ==19) {
+                        boardComp[compboardcounter] = deck.getDeckcompSeletcted()[i];
+                        compsum += deck.getDeckcompSeletcted()[i].getNum();
+                        compboardcounter++;
+                        deck.getDeckcompSeletcted()[i] = null;
+                    }
                 }
             }
-        }
-        boolean x = true;
-        while(x==true) {
-            if(deck.getDeck()[counter] != null) {
-                boardComp[compboardcounter] = deck.getDeck()[counter];
-                compsum += boardComp[compboardcounter].getNum();
-                deck.getDeck()[counter] = null;
-                compboardcounter++;
-                x=false;
+            boolean x = true;
+            while(x==true) {
+                if(deck.getDeck()[counter] != null) {
+                    boardComp[compboardcounter] = deck.getDeck()[counter];
+                    compsum += boardComp[compboardcounter].getNum();
+                    deck.getDeck()[counter] = null;
+                    compboardcounter++;
+                    x=false;
+                }
+                counter++;
             }
-            counter++;
+            System.out.println();
+            System.out.println("***************************");
+            System.out.println("Computer Board");
+            System.out.println("---------------------------");
+            for (int i = 0; i < boardComp.length ; i++) {
+                if(boardComp[i] != null) {
+                    if (boardComp[i].getColor() == null) {
+                        System.out.print(boardComp[i].getSign()+ " ");
+                    }
+                    System.out.print(boardComp[i].getColor() + boardComp[i].getNum() + boardComp[i].getSign()+" ");
+                }
+            }
+            System.out.println();
+            System.out.println("*************************");
+
         }
+    }
+
+    public void EndGame() {
+
         System.out.println();
         System.out.println("***************************");
         System.out.println("Computer Board");
@@ -268,35 +313,58 @@ public class Game {
                 System.out.print(boardComp[i].getColor() + boardComp[i].getNum() + boardComp[i].getSign()+" ");
             }
         }
-        System.out.println();
-        System.out.println("*************************");
-        if(compsum > 16) {
-            EndGame();
-        } else {
-            Play();
-        }
-    }
 
-    public void EndGame() {
-        if(playersum == compsum) {
-            win.setCompwin(1);
-            win.setPlayerwin(1);
-            System.out.println("IT IS DRAW");
-            System.out.println("NEW GAME IS STARTING");
-        } else if(playersum > compsum && playersum < 21) {
-            win.setPlayerwin(1);
-        } else if(compsum > playersum && compsum < 21) {
-            win.setCompwin(1);
-        }
-        if(win.getCompwin()==3 || win.getPlayerwin()==3) {
-            if (win.getCompwin()> win.getPlayerwin()) {
-                System.out.println("Computer won the game");
+        System.out.println("Computer cards are: ");
+
+        for (int i = 0; i < deck.getDeckcompSeletcted().length ; i++) {
+            if (deck.getDeckplayerSeletcted()[i] == null) {
+                System.out.print((i+1) + ")"+"O ");
             } else {
-                System.out.println("You won the game");
-
+                System.out.print((i+1) + ")"+"X ");
             }
+        }
+        //---------------------------------------------------------------------------------
+        if(playersum == compsum) {
+
+            System.out.println("IT IS DRAW");
+            System.out.println(compsum);
+            System.out.println(playersum);
+            win.setPlayerwin();
+            win.setCompwin(1);
+        } else if(playersum > compsum && playersum < 21) {
+            System.out.println("Score is-> " + "Player: " + playersum + "Comp: " + compsum);
+            System.out.println(win.getCompwin());
+            System.out.println(win.getPlayerwin());
+            win.setPlayerwin();
+
+
+        } else if(compsum > playersum && compsum < 21) {
+
+            System.out.println("Score is-> " + "Player: " + playersum + "Comp: " + compsum);
+            System.out.println(win.getCompwin());
+            System.out.println(win.getPlayerwin());
+            win.setCompwin(win.getCompwin() + 1);
 
         }
+        else if(playersum>20){
+            win.setCompwin(win.getCompwin() + 1);
+            System.out.println(win.getCompwin());
+            System.out.println(win.getPlayerwin());
+
+            System.out.println("Score is-> " + "Player: " + playersum + "Comp: " + compsum);
+            System.out.println("--WİN COMPUTER--");
+        }
+        else if(compsum>20){
+            System.out.println("Score is-> " + "Player: " + playersum + "Comp: " + compsum);
+            System.out.println("--WİN PLAYER--");
+            System.out.println(win.getCompwin());
+            System.out.println(win.getPlayerwin());
+            win.setPlayerwin ();
+
+        }
+        System.out.println("NEW GAME IS STARTING");
+    }
+    public void Clear()  {
         playersum =0;
         compsum=0;
         counter =0;
@@ -307,6 +375,15 @@ public class Game {
             boardplayer[i] = null;
             boardComp[i]=null;
         }
+    }
+    public void Finish() {
+        if (win.getCompwin() == 3 || win.getPlayerwin() == 3) {
+            if (win.getCompwin() > win.getPlayerwin()) {
+                System.out.println("COMPUTER WON");
+            } else {
+                System.out.println("PLAYER WON");
 
+            }
+        }
     }
 }
